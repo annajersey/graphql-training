496ffcc304fd8be167ba26c7fab92f79a1293634
anna <anna.brodskaya@gmail.com>
2019-09-21 18:11:47 +0300

    Added mutation

--- index.js
+++ index.js
@@ -4,19 +4,39 @@ import schema from './schema';
 
 const app = express();
 
-app.get('/', (req,res) => {
+app.get('/', (req, res) => {
     res.send("test")
 });
-const root = {friend: () => {
-    return{
-        "id": 17,
-        "firstName": "qwe",
-        "lastName": "yu",
-        "gender": "jfgj",
-        "language": "gjfgh",
-        "email": "hgfcx"
+
+class Friend{
+    constructor(id, {firstName, lastName, gender, language, email}){
+        this.id = id;
+        this.lastName = lastName;
+        this.firstName = firstName
+        this.language = language;
+        this.email = email;
+        this.gender = gender;
     }
-    }}
+}
+
+const friendDatabase = {}
+const root = {
+    friend: () => {
+        return {
+            "id": 17,
+            "firstName": "qwe",
+            "lastName": "yu",
+            "gender": "jfgj",
+            "language": "gjfgh",
+            "email": "hgfcx"
+        }
+    },
+    createFriend: ({input}) => {
+        let id = require('crypto').randomBytes(10).toString('hex');
+        friendDatabase[id] = input;
+        return new Friend(id, input);
+    }
+}
 app.use('/graphqltest', graphqlHTTP({
     schema,
     rootValue: root,
